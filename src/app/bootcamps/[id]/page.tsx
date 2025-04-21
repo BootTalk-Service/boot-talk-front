f00ca2f6eaab82275072b4f04ecb07776b2731f9
방주영 <joo93d.o@gmail.com>
2025-04-22 02:12:07 +0900

    :recycle: params 타입 추가

--- src/app/bootcamps/[id]/page.tsx
+++ src/app/bootcamps/[id]/page.tsx
@@ -1,15 +1,13 @@
 import BootcampDetailClient from "@/components/feature/detail/BootcampDetailClient";
 
-interface PageProps {
+export default function BootcampDetailPage({
+  params,
+}: {
   params: { id: string };
-}
-
-const BootcampDetailPage = async ({ params }: PageProps) => {
+}) {
   if (!params.id) {
     return <div>Invalid Bootcamp ID</div>;
   }
 
   return <BootcampDetailClient id={params.id} />;
-};
-
-export default BootcampDetailPage;
+}
