c4c6705dddbc87bec8729d4a2de2a95d7c3eea4a
Son-Youjin <wlsl423@naver.com>
2025-04-22 19:53:47 +0900

    :sparkles: 상세페이지 리뷰 섹션 연동

--- src/components/feature/detail/BootcampDetailClient.tsx
+++ src/components/feature/detail/BootcampDetailClient.tsx
@@ -1,21 +1,25 @@
 "use client";
 
 import { useGetBootcampDetail } from "@/hooks/detail/useGetBootcampDetail";
+import { useGetBootcampReviews } from "@/hooks/detail/useGetBootcampreviews";
 import BootcampDetailInfo from "@/components/feature/detail/BootcampDetailInfo";
 import BootcampSchedule from "@/components/feature/detail/BootcampSchedule";
 import BootcampReview from "@/components/feature/detail/BootcampReview";
 import BootcampIntro from "./BootcampIntro";
-
 interface Props {
   id: string;
 }
 
 const BootcampDetailClient = ({ id }: Props) => {
   const { data, isLoading, isError } = useGetBootcampDetail(id);
+  const {
+    data: reviewsData,
+    isLoading: isReviewsLoading,
+    isError: isReviewsError,
+  } = useGetBootcampReviews(id);
 
   if (isLoading) return <p className="p-10 text-center text-gray-500">불러오는 중...</p>;
   if (isError || !data) return <p className="p-10 text-center text-gray-500">데이터를 불러오지 못했습니다.</p>;
-
   return (
     <>
     <main className="max-w-screen-xl min-w-[320px] mx-auto px-6 py-10">
@@ -29,7 +33,6 @@ const BootcampDetailClient = ({ id }: Props) => {
         </h1>
         <div className="border-t border-amber-600 mt-4" />
       </div>
-
       {/* 상세 정보 카드 */}
       <BootcampDetailInfo
         trainingCenterName={data.trainingCenterName}
@@ -38,7 +41,6 @@ const BootcampDetailClient = ({ id }: Props) => {
         trainingCenterEmail={data.trainingCenterEmail  || "-"}
         trainingCenterUrl={data.trainingCenterUrl}
       />
-
       <BootcampSchedule
         bootcampStartDate={data.bootcampStartDate}
         bootcampEndDate={data.bootcampEndDate}
@@ -49,10 +51,11 @@ const BootcampDetailClient = ({ id }: Props) => {
 
       <BootcampIntro />
 
-      <BootcampReview reviews={data.reviews} />
+      {!isReviewsLoading && !isReviewsError && reviewsData && (
+        <BootcampReview reviews={reviewsData ?? []} />
+      )}
     </main>
     </>
   );
 };
-
 export default BootcampDetailClient;
