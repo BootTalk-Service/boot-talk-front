4b07874afef49f84928e599e4ea72f51b86c9374
Son-Youjin <wlsl423@naver.com>
2025-04-23 00:33:59 +0900

    :recycle: SSE 알림 수신 수정

--- src/hooks/notification/useNotificationEffect.ts
+++ src/hooks/notification/useNotificationEffect.ts
@@ -13,7 +13,7 @@ export function useNotificationEffect() {
       withCredentials: true,
     });
 
-    evtSource.onmessage = (event) => {
+    const handleNotification = (event: MessageEvent) => {
       try {
         const data = JSON.parse(event.data);
         addNotification(data);
@@ -23,12 +23,15 @@ export function useNotificationEffect() {
       }
     };
 
+    evtSource.addEventListener("notification", handleNotification);
+    evtSource.onmessage = handleNotification;
     evtSource.onerror = (err) => {
       console.error("SSE 연결 에러:", err);
       evtSource.close();
     };
 
     return () => {
+      evtSource.removeEventListener("notification", handleNotification);
       evtSource.close();
     };
   }, [addNotification, incrementUnread]);
