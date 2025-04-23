5a742490e45ab75eef8f508fa178356414abae39
Son-Youjin <wlsl423@naver.com>
2025-04-23 20:07:12 +0900

    ♻️ 로그인 및 관심직무 레이아웃 수정

--- src/components/common/AuthCard.tsx
+++ src/components/common/AuthCard.tsx
@@ -5,8 +5,8 @@ interface AuthCardProps {
 const AuthCard = ({ children }: AuthCardProps) => {
   return (
     <main className="flex min-h-screen items-center justify-center px-4 bg-base-100">
-      <div className="card w-full max-w-md min-h-[460px] bg-white border border-gray-200 shadow-md rounded-xl">
-        <div className="card-body p-8 flex items-center justify-center w-full">
+      <div className="card w-full max-w-sm min-h-[560px] bg-white border border-gray-200 shadow-md rounded-xl">
+        <div className="card-body p-8 flex flex-col justify-center w-full">
           {children}
         </div>
       </div>
